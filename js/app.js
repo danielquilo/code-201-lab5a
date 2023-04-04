69883a2c1919f0d7166462f3a1ef438d61074a73
danielquilo <danielquilo1012@yahoo.com>
2023-04-04 10:42:10 +0100

    done

--- js/app.js
+++ js/app.js
@@ -8,13 +8,18 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-
+function sum(a, b) { 
+//eslint-disable-line 
+const theSum = a + b;
+const theString = "The sum of " + a + " and " + b + " is " + theSum + ".";  
+// console.log(theString);    
+// const theBetterString = `The sum of ${a and ${b} is £{theSum}. `; }
+return [theSum, theString];
 }
 
+//console.log(sum());
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
-
+testSum(4, 7);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -26,12 +31,17 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a, b) {
+//eslint-disable-line
+const theMultiSum = a * b;
+const theMultiString = `The product of ${a} and ${b} is ${theMultiSum}.`;
+return [theMultiSum, theMultiString];
 
+//console.log(multiply(a, b));
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -47,12 +57,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+function sumAndMultiply(a, b, c) { 
+//eslint-disable-line
+const theSum = sum(a, b);
+const theSumOf = sum(theSum[0], c);
+const theMulti = multiply(a, b);
+const theMultiOf = multiply(theMulti[0], c);
+const theSumString = `${a} and ${b} and ${c} sum to ${theSumOf[0]}.`;
+const theMultiString = `The product of ${a} and ${b} and ${c} is ${theMultiOf[0]}.`;
+return [theSumOf[0], theMultiOf[0], theSumString, theMultiString];
 }
 
+//console.log(SumAndMultiply());
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -69,13 +88,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
 
+function sumArray(sumArr) {
+const sums = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
+return [sums, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sums} is their sum.`];
 }
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -91,11 +113,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+//const multi = multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0];
+//return [multi, `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of{multi}.`];
 
+
+const theProduct = multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0];
+const theProductString = "The numbers " + multArr[0] + "," + multArr[1] + "," + multArr[2] + " have a product of " + theProduct + ".";
+return [theProduct, theProductString];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -119,10 +147,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+let product = dynamicArray[0];
+let nums = dynamicArray[0];
+for (let i = 0; i < dynamicArray.length - 1; i++) {
+product = multiply(product, dynamicArray[i + 1])[0];
+nums = nums + "," + dynamicArray[i + 1];
+}
+let productString = `The numbers ${nums} have a product of ${product}.`;
+return [product, productString];
 }
 
+
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
